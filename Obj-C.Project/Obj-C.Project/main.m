9f76693b1237005065540d08570da708455626be
2jumper3 <2jumper3@gmail.com>
2019-07-10 12:01:52 +0300

    first

---
+++ Obj-C.Project/Obj-C.Project/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Obj-C.Project
+//
+//  Created by Sergey on 10/07/2019.
+//  Copyright © 2019 Sergey. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
